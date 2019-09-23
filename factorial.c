e6dcc1d9ac41c0e42ae342545697a3a50e7d13e5
dabinaseo <lemonstar99@korea.ac.kr>
2019-09-23 20:07:45 +0900

    edit factorial.c

--- factorial.c
+++ factorial.c
@@ -2,6 +2,10 @@
 
 int main(){
     int num = 6;
-    int product = 720;
+    int i;
+    int product = 1;
+
+    for (i =1; i<=num; i++) product *= i;
+        
     printf("Factorial of %d is %d\n", num, product);
 }
